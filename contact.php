0467fc0058db69de8139af536315653b1e2c3140
Simeon <simogeorgiew@gmail.com>
2020-03-24 20:15:45 +0200

    New addition

--- contact.php
+++ contact.php
@@ -2,55 +2,55 @@
 
 <?php
 
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
 
-$email_from = $visitor_email;
+    $email_from = $visitor_email;
 
-$email_subject = "Contacting";
+    $email_subject = "Portfolio message";
 
-$email_body = "You have received a new message from the user $name.\n".
-"Here is the message:\n $message".
+    $email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
 
-$to = "simogeorgiew@gmail.com";
+    $to = "simogeorgiew@gmail.com";
 
-$headers = "From: $email_from \r\n";
+    $headers = "From: $email_from \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+    mail($to,$email_subject,$email_body,$headers);
 
-// Securing from email injection
+    // Securing from email injection
 
-function IsInjected($str)
-{
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-               
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-    
-    if(preg_match($inject,$str))
+    function IsInjected($str)
     {
-      return true;
+        $injections = array('(\n+)',
+            '(\r+)',
+            '(\t+)',
+            '(%0A+)',
+            '(%0D+)',
+            '(%08+)',
+            '(%09+)'
+            );
+                
+        $inject = join('|', $injections);
+        $inject = "/$inject/i";
+        
+        if(preg_match($inject,$str))
+        {
+        return true;
+        }
+        else
+        {
+        return false;
+        }
     }
-    else
-    {
-      return false;
-    }
-}
 
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
+    if(IsInjected($visitor_email))
+    {
+        echo "Bad email value!";
+        exit;
+    }
 
 ?>
